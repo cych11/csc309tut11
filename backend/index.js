39692e790e17171cb32763a32582cd456c1cc511
chungcyr <cyrus.chung@mail.utoronto.ca>
2025-11-21 02:06:07 -0500

    let me sleep

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,22 @@
+// backend/index.js
 import express from "express";
-import routes from "./routes.js";
 import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
+
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(cors({
   origin: FRONTEND_URL,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
-  credentials: true
+  credentials: true,
 }));
 
-app.options("*", cors({
-  origin: FRONTEND_URL,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"],
-  credentials: true
-}));
+app.options("*", cors());
 
 app.use(express.json());
-
 app.use("/", routes);
 
 export default app;
